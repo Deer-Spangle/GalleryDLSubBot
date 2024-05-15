e8b14c8d3cecb05f1af3935328ca94d05e011271
Deer-Spangle <deer@spangle.org.uk>
2024-05-15 19:25:45 +0100

    Handle all links at once, post all images or ask if user wants to download a zip or subscribe. Implement zip download

--- gallery_dl_sub_bot/bot.py
+++ gallery_dl_sub_bot/bot.py
@@ -1,10 +1,14 @@
+import asyncio
 import html
 import logging
 import re
+import shutil
+import uuid
 
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, Button
 
 from gallery_dl_sub_bot.gallery_dl_manager import GalleryDLManager
+from gallery_dl_sub_bot.hidden_data import parse_hidden_data, hidden_data
 from gallery_dl_sub_bot.link_fixer import LinkFixer
 
 logger = logging.getLogger(__name__)
@@ -26,6 +30,7 @@ class Bot:
         self.client.add_event_handler(self.start, events.NewMessage(pattern="/start", incoming=True))
         self.client.add_event_handler(self.boop, events.NewMessage(pattern="/beep", incoming=True))
         self.client.add_event_handler(self.check_for_links, events.NewMessage(incoming=True))
+        self.client.add_event_handler(self.handle_zip_callback, events.CallbackQuery(pattern="dl_zip:"))
         # Start listening
         try:
             logger.info("Starting bot")
@@ -61,7 +66,52 @@ class Bot:
         # Tell the user the links
         await event.respond("Found these links:\n" + "\n".join(html.escape(l) for l in fixed_links), parse_mode="html")
         # Check them in gallery-dl
-        for link in fixed_links:
-            resp = await self.dl_manager.run(["--dump-json", link])
-            await event.respond(f"Gallery DL said:\n<pre>{html.escape(resp)}</pre>", parse_mode="html")
+        await asyncio.gather(*(self._handle_link(link, event) for link in fixed_links))
         raise events.StopPropagation
+
+    async def _handle_link(self, link: str, event: events.NewMessage.Event) -> None:
+        dl_path = f"store/downloads/{uuid.uuid4()}/"
+        evt = await event.respond(f"⏳ Downloading link: {html.escape(link)}", parse_mode="html")
+        try:
+            # TODO: queueing
+            # TODO: in progress message
+            resp = await self.dl_manager.run(["--write-metadata", "--write-info-json", "-d", dl_path, link])
+        except Exception as e:
+            logger.error(f"Failed to download link {link}", exc_info=e)
+            await evt.respond(f"Failed to download link {html.escape(link)} :(")
+            raise e
+        lines = resp.strip().split("\n")
+        await evt.respond(f"Downloaded {len(lines)} images(s)", parse_mode="html")
+        await evt.delete()
+        if len(lines) < 10:
+            await evt.respond(f"{html.escape(link)}", parse_mode="html", file=lines)
+        else:
+            await evt.respond(
+                f"Would you like to download these files as a zip?{hidden_data({'path': dl_path})}",
+                parse_mode="html",
+                buttons=[
+                    [Button.inline("Yes", "dl_zip:yes")],
+                    [Button.inline("No thanks", "dl_zip:no")],
+                ]
+            )
+            await evt.respond(f"Would you like to subscribe to {html.escape(link)}?", parse_mode="html")  # TODO
+
+    async def handle_zip_callback(self, event: events.CallbackQuery.Event) -> None:
+        query_data = event.query.data
+        query_resp = query_data.removeprefix(b"dl_zip:")
+        logger.info(f"Callback query pressed: {query_data}")
+        menu_msg = await event.get_message()
+        menu_data = parse_hidden_data(menu_msg)
+        dl_path = menu_data["path"]
+        if query_resp == b"no":
+            await menu_msg.delete()
+            logger.info(f"Removing download path: {dl_path}")
+            shutil.rmtree(dl_path)
+            raise events.StopPropagation
+        if query_resp == b"yes":
+            await menu_msg.edit("⏳ Creating zip archive...", buttons=None)
+            zip_path = f"store/downloads/{uuid.uuid4()}"
+            shutil.make_archive(zip_path, "zip", dl_path)
+            await menu_msg.respond("Here is the zip archive of that feed", file=f"{zip_path}.zip")
+            await menu_msg.delete()
+            raise events.StopPropagation
